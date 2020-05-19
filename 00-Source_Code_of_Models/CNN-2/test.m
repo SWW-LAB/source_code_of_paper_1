c7e3bf23f365f73bfb8f5770b295908dfc1a1578
SWW-LAB <57850488+SWW-LAB@users.noreply.github.com>
2020-05-19 11:49:22 +0800

    Update test.m

--- 00-Source_Code_of_Models/CNN-2/test.m
+++ 00-Source_Code_of_Models/CNN-2/test.m
@@ -5,11 +5,11 @@ load cnn_deep;
 
 
 figure(2)
-plot(cnn_deep.test_r);%训练过程中的测试集loss
-%title('测试集loss');
+plot(cnn_deep.test_r);
+%title('test loss');
 figure(3)
-plot(cnn_deep.verif_r);%训练过程中的验证集loss，用于停止训练，确定迭代次数
-%title('验证集loss');
+plot(cnn_deep.verif_r);
+%title('valid loss');
 figure(4)
-plot(cnn_deep.tra_r);%训练过程中的训练集loss
-%title('训练集loss');
+plot(cnn_deep.tra_r);
+%title('train loss');
