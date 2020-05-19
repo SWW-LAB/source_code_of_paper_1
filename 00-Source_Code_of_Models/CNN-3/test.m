d425ac005f079170e674a44b088cbc5bc76112d6
SWW-LAB <57850488+SWW-LAB@users.noreply.github.com>
2020-05-19 14:38:27 +0800

    Update test.m

--- 00-Source_Code_of_Models/CNN-3/test.m
+++ 00-Source_Code_of_Models/CNN-3/test.m
@@ -4,14 +4,13 @@ clc;
 load cnn1;
 
 figure(1)
-plot(cnn1.rL);  %训练过程中的均方误差
+plot(cnn1.rL);
 title('pricing function - Smoothing mean square error');
 figure(2)
-plot(cnn1.test_r);%训练过程中的测试集loss
-%title('测试集loss');
+plot(cnn1.test_r);
+
 figure(3)
-plot(cnn1.verif_r);%训练过程中的验证loss，用于停止训练，确定迭代次数
-%title('验证集loss');
+plot(cnn1.verif_r);
+
 figure(4)
-plot(cnn1.tra_r);%训练过程中的训练集loss
-%title('训练集loss');
+plot(cnn1.tra_r);
