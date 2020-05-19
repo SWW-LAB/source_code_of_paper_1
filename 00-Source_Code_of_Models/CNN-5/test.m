162af06c927aa980693ac01257b312ca39c7937c
SWW-LAB <57850488+SWW-LAB@users.noreply.github.com>
2020-05-19 15:58:19 +0800

    Update test.m

--- 00-Source_Code_of_Models/CNN-5/test.m
+++ 00-Source_Code_of_Models/CNN-5/test.m
@@ -4,11 +4,10 @@ clc;
 load cnn_multi_finger;
 
 figure(2)
-plot(cnn_multi_finger.test_r);%训练过程中的测试集loss
-%title('测试集loss');
+plot(cnn_multi_finger.test_r);
+
 figure(3)
-plot(cnn_multi_finger.verif_r);%训练过程中的验证集loss，用于停止训练，确定迭代次数
-%title('验证集loss');
+plot(cnn_multi_finger.verif_r);
+
 figure(4)
-plot(cnn_multi_finger.tra_r);%训练过程中的训练集loss
-%title('训练集loss');
+plot(cnn_multi_finger.tra_r);
