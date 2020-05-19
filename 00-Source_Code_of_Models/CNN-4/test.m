5582c9bea22a18e25e4d4bf57b1ab6ee4567a8b7
SWW-LAB <57850488+SWW-LAB@users.noreply.github.com>
2020-05-19 15:46:46 +0800

    Update test.m

--- 00-Source_Code_of_Models/CNN-4/test.m
+++ 00-Source_Code_of_Models/CNN-4/test.m
@@ -4,11 +4,10 @@ clc;
 load cnn2;
 
 figure(2)
-plot(cnn2.test_r);%训练过程中的测试集loss
-%title('测试集loss');
+plot(cnn2.test_r);
+
 figure(3)
-plot(cnn2.verif_r);%训练过程中的验证loss，用于停止训练，确定迭代次数
-%title('验证集loss');
+plot(cnn2.verif_r);
+
 figure(4)
-plot(cnn2.tra_r);%训练过程中的训练集loss
-%title('训练集loss');
+plot(cnn2.tra_r);
